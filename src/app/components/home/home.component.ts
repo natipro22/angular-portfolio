40e250009cda960cff75e6e79ee978b7a8b88a87
Natnael Yirga <115155522+natipro22@users.noreply.github.com>
2024-03-14 08:18:14 +0300

    tt

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,18 +5,12 @@ import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(
-    private analyticsService: AnalyticsService,
-  ) {
-  }
+  constructor(private analyticsService: AnalyticsService) {}
 
   ngOnInit(): void {
-    this.analyticsService.sendAnalyticPageView("/inicio", "Se entro a inicio")
+    // this.analyticsService.sendAnalyticPageView('/inicio', 'Se entro a inicio');
   }
-
-
 }
--- src/app/services/language/language.service.ts
+++ src/app/services/language/language.service.ts
@@ -3,32 +3,31 @@ import { TranslateService } from '@ngx-translate/core';
 import { Location } from '@angular/common';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LanguageService {
-
-  language: "es" | "en";
+  language: 'am' | 'en';
 
   constructor(
     public translateService: TranslateService,
-    private location: Location,
+    private location: Location
   ) {}
 
-  initLanguage(){
-    this.translateService.addLangs(["en", "es"])
+  initLanguage() {
+    this.translateService.addLangs(['en', 'am']);
     let language = navigator.language || (navigator as any).userLanguage;
-    language = language.split("-").includes("es") ? "es" : "en"
-    this.translateService.setDefaultLang(language)
+    language = language.split('-').includes('am') ? 'am' : 'en';
+    this.translateService.setDefaultLang(language);
 
     // Change the URL without navigate:
-    this.location.go(language)
+    this.location.go(language);
 
-    this.language=language
+    this.language = language;
   }
 
-  changeLanguage(language){
-    this.translateService.setDefaultLang(language)
-    this.location.go(language)
-    this.language=language
+  changeLanguage(language) {
+    this.translateService.setDefaultLang(language);
+    this.location.go(language);
+    this.language = language;
   }
 }
